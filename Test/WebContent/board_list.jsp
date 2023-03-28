caf20a66b71a4670c7cbf5998218c0fba5079f28
Goodcheese92 <dltjdwls555@gmail.com>
2023-03-28 17:44:00 +0900

    부트스트랩 + 게시판 연습

---
+++ Test/WebContent/board_list.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>board_list.jsp</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/jquery-3.6.4.min.js"></script>
+	</head>
+	
+	<body>
+		<table>
+			
+		</table>	
+	</body>
+</html>
---
+++ Test/src/action/BoardListAction.java
@@ -0,0 +1,33 @@
+package action;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BoardDAO;
+import vo.BoardVO;
+
+/**
+ * Servlet implementation class BoardListAction
+ */
+@WebServlet("/board_list.do")
+public class BoardListAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<BoardVO> board_list = BoardDAO.getInstance().select();
+		
+		request.setAttribute("board_list", board_list);
+		
+		RequestDispatcher disp = request.getRequestDispatcher("board_list.jsp");
+		disp.forward(request, response);
+		
+	} // end of service
+
+} // end of class
